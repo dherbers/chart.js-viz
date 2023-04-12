87839990d73450f0b9d54199d0f08190e3a9ae05
dherbers <dherbers@umd.edu>
2023-04-12 13:57:39 -0400

    create bar chart

--- javascript.js
+++ javascript.js
@@ -2,20 +2,13 @@ var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['2015', '2016', '2017', '2018', '2019', '2020'],
+        labels: ['America', 'East Asia & Pacific', 'Europe & Central Asia', 'Middle East & North Africa', 'South Asia', 'Sub-Saharan Africa'],
         datasets: [{
-            label: '# of Apples',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'average GDP per capita',
+            data: [18791.13, 14480.30, 20818.45, 13856.00, 2505.17, 2569.00],
             backgroundColor: [
-                '#ff0800'
+                '#4AA63C'
             ]
-        },
-        {
-            label: '# of Oranges',
-            data: [22, 10, 23, 6, 22, 13],
-            backgroundColor: [
-                '#FFA500'
-            ],
         }
     ]
     },
@@ -24,14 +17,14 @@ var myChart = new Chart(ctx, {
         plugins: {
             title: {
                 display: true,
-                text: 'Apples and Oranges by year',
+                text: 'GDP per Capita by Region',
                 font: {
                     size: 18
                 }
             },
             subtitle: {
                 display: true,
-                text: 'For all but one year, oranges outnumbered apples'
+                text: 'America has the highest average GDP, followed by East Asia and the Pacific.'
             }
         },
         scales: {
